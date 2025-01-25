f2181f4c1cc8440e9536d35f702ec5b4b1ce4f3b
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2025-01-25 17:47:11 -0500

    Fix Bullet Points

--- react-app/src/Components/Home.jsx
+++ react-app/src/Components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <>
         <Navbar />
         <div className="home">
-            <h1>Home</h1>
+            <h1 className="FunFact">Fun Fact</h1>
         </div>
         </>
     );
