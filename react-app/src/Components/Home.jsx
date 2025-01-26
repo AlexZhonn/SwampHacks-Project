f526bc1a26692617cc906bc680b2ab8b894fe01a
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2025-01-25 21:33:22 -0500

    Fixed Home Page

--- react-app/src/Components/Home.jsx
+++ react-app/src/Components/Home.jsx
@@ -5,8 +5,17 @@ const Home = () => {
         <>
         <Navbar />
         <div className="home">
-            <h1 className="FunFact">Fun Fact</h1>
+            <h1 className="FunFact"> 
+                Fun Fact: 
+            </h1>
         </div>
+        <div className="block">
+             Welcome to the Website!
+            <div className="blockText">
+                <img className="img" src="../recycling.jpg"  align="center"/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            </div>
+        </div> 
         </>
     );
 };
